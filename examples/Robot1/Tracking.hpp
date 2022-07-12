e6567b4017eee2f62cd55dd2e43404b912b8c545
Lake Hu <hudasha@scu.edu.cn>
2022-07-12 16:56:45 +0800

    amend by Xubo

--- examples/Robot1/Tracking.hpp
+++ examples/Robot1/Tracking.hpp
@@ -27,6 +27,16 @@ typedef struct {
     uint index;             // 目标临时编号
     char redis_key[40];     // UUID
     int intensity;        // 目标激光强度总和
+
+    float pt1_x;
+    float pt1_y;
+    float pt2_x;
+    float pt2_y;
+    float pt3_x;
+    float pt3_y;
+    float pt4_x;
+    float pt4_y;
+
     float width;            // 精度：0.01m
     float length;           // 精度：0.01m
     float height;           // 精度：0.01m
@@ -158,6 +168,7 @@ public:
 
         qint64 kalman_t1 = QDateTime::currentMSecsSinceEpoch(); //TEST***
         size_t left_size = prevTargets_.size();
+        if (matching.size() < left_size) return; // add for crash
         for (size_t i = 0; i < left_size; ++i) {
             int right = matching[i];
             auto eit = std::find_if(edges.begin(), edges.end(), findEdges(i, right));
