5c654ea2e7a3ac6b4dddb7cb3332875d09728a6d
Lake Hoo <leonshark@hotmail.com>
2022-07-13 11:41:26 +0800

    目标控制参数

--- examples/Robot1/CDataProcessor.cpp
+++ examples/Robot1/CDataProcessor.cpp
@@ -3,13 +3,15 @@
 #include <QJsonObject>
 #include <QDateTime>
 #include <QJsonValue>
+#include <math.h>
 #include "IDataDispatcher.h"
 
 int KalmanTracking::LidarTracking::frames = 0;
 
 #include "CommonDefine.h"
 CDataProcessor::CDataProcessor(CPluginContext *pContext, IInterfaceManager *pInterfaceManager)
-    : m_pContext(pContext)
+    : QObject(nullptr)
+    , m_pContext(pContext)
     , m_pInterfaceManager(pInterfaceManager)
 {
     // TOOD: 算法本身，需要在这里，从插件的配置文件中，将需要的参数解析出来，
@@ -34,6 +36,28 @@ bool CDataProcessor::Init()
             qWarning() << Q_FUNC_INFO << QString::fromUtf8("redis客户端初始化失败");
             m_pRedisClient = nullptr;
         }
+
+        // 轨迹不可见时，不启动跟踪服务
+//        {
+//            QByteArray ba_in;
+//            if (!m_pRedisClient->Get("TrackVisible", ba_in))
+//            {
+//                qWarning() << Q_FUNC_INFO << QString::fromUtf8("redis字符串TrackVisible非true，不允许启动跟踪服务");
+//                break;
+//            }
+
+//            QString strIn = QString::fromUtf8(ba_in);
+//            if (strIn != "true")
+//            {
+//                qWarning() << Q_FUNC_INFO << QString::fromUtf8("redis字符串TrackVisible非true，不允许启动跟踪服务");
+//                break;
+//            }
+//        }
+
+        m_pTimer = new QTimer();
+        connect(m_pTimer, &QTimer::timeout, this, &CDataProcessor::slotForTimeout);
+        m_pTimer->start(3000);
+
         ret = true;
     } while (false);
     return ret;
@@ -43,6 +67,12 @@ bool CDataProcessor::Uninit()
 {
     bool ret = false;
     do {
+        if (m_pTimer != nullptr)
+        {
+            m_pTimer->stop();
+            m_pTimer->deleteLater();
+            m_pTimer = nullptr;
+        }
         if (m_pRedisClient != nullptr)
         {
             m_pRedisClient->Uninit();
@@ -54,10 +84,10 @@ bool CDataProcessor::Uninit()
     return ret;
 }
 
-static bool verifyData(Sin *pin)
+static bool verifyData(SIn *pin)
 {
     bool ret = false;
-    if (pin->m_obj_num <= 0) 
+    if (pin->m_obj_num <= 0)
         return ret;
     for (int i = 0; i < pin->m_obj_num; ++i) {
         PV_OBJ_DATA *tp = pin->m_obj_data + i;
@@ -95,9 +125,15 @@ bool CDataProcessor::ProcessData(const QByteArray &in)
             break;
         }
 
+        if (!m_trackVisible)
+        {
+            // 不执行跟踪计算
+            break;
+        }
+
         // 注意，这里的数据结构中，不能有指针，否则不能与 QByteArray 相互转换
         SIn* pIn = (SIn*)in.data();
-        if (!verifyData(pin)) {
+        if (!verifyData(pIn)) {
             // 没有目标或异常条件下返回false时
             break;
         }
@@ -108,7 +144,7 @@ bool CDataProcessor::ProcessData(const QByteArray &in)
         // 加入Set并按照index排序
         inSet.insert(inSet.end(), pIn->m_obj_data, pIn->m_obj_data+pIn->m_obj_num);
         if (lidar_ == nullptr) {
-            lidar_ = new KalmanTracking::LidarTracking(inSet,m_param);
+            lidar_ = new KalmanTracking::NoneTracking(inSet,m_param);
         }
         else {
             lidar_->tracking(inSet);
@@ -162,8 +198,77 @@ bool CDataProcessor::ProcessData(const QByteArray &in)
 
 #endif
         // 如果需要处理 SOut1或者 SOut2 的数据, 需要向数据分发器订阅数据，方法为:
-//        pDataDispatcher->Register(p, "SOut1");
+        //        pDataDispatcher->Register(p, "SOut1");
         // 需要在 CPluginDemo::RegistInterfaces 中注册 对应类型的数据
+
+        ret = true;
+    } while (false);
+    return ret;
+}
+
+void CDataProcessor::slotForTimeout()
+{
+    do {
+        // 获取是否执行跟踪计算功能
+        {
+            QByteArray ba_in;
+            if (!m_pRedisClient->Get("TrackVisible", ba_in))
+            {
+                qWarning() << Q_FUNC_INFO << QString::fromUtf8("redis字符串找不到TrackVisible");
+                break;
+            }
+
+            QString strIn = QString::fromUtf8(ba_in);
+            if (strIn == "false")
+            {
+                qWarning() << Q_FUNC_INFO << QString::fromUtf8("redis字符串TrackVisible非true，结束跟踪服务");
+                break;
+            }
+            m_trackVisible = true;
+        }
+
+        // 获取跟踪计算算法参数
+        if (!UpdateTrackParams())
+        {
+            break;
+        }
+
+    } while (false);
+}
+
+bool CDataProcessor::UpdateTrackParams()
+{
+    bool ret = false;
+    do {
+
+        QByteArray baTrackParams;
+        if (!m_pRedisClient->Get("TrackParams", baTrackParams))
+        {
+            qWarning() << Q_FUNC_INFO << QString::fromUtf8("redis字符串找不到TrackParams");
+            break;
+        }
+
+        QJsonDocument jdTrackParams = QJsonDocument::fromJson(baTrackParams);
+        if (!jdTrackParams.isNull())
+        {
+            qWarning() << Q_FUNC_INFO << QString::fromUtf8("redis字符串TrackParams不是合法的json");
+            break;
+        }
+        QJsonObject joTraceParam = jdTrackParams.object();
+
+        m_algParamDistanceVector = joTraceParam.value("DistanceVector").toString();                             //!< 距离向量 缺省是X，Y值，可选 X轴速度，Y轴速度，强度。
+        m_algParamDistanceLimit = joTraceParam.value("DistanceLimit").toString().toDouble();                    //!< 距离阈值
+        m_algParamInitVariancePosX = joTraceParam.value("InitVariancePosX").toString().toDouble();              //!< 初始方差-X轴<initVariancePosX>：0.1
+        m_algParamInitVariancePosY = joTraceParam.value("InitVariancePosY").toString().toDouble();              //!< 初始方差-Y轴<initVariancePosY>：0.1
+        m_algParamInitVariancePosZ = joTraceParam.value("InitVariancePosZ").toString().toDouble();              //!< 初始方差-Z轴<initVariancePosZ>：0.0
+        m_algParamInitVarianceVelX = joTraceParam.value("InitVarianceVelX").toString().toDouble();              //!< 初始方差-X轴速度<initVarianceVelX>：10
+        m_algParamInitVarianceVelY = joTraceParam.value("InitVarianceVelY").toString().toDouble();              //!< 初始方差-Y轴速度<initVarianceVelY>：10
+        m_algParamInitVarianceVelZ = joTraceParam.value("InitVarianceVelZ").toString().toDouble();              //!< 初始方差-Z轴速度<initVarianceVelZ>：0.0
+        m_algParamInitVarianceAccX = joTraceParam.value("InitVarianceACCX").toString().toDouble();              //!< 初始方差-X轴加速度<initVarianceAccX>：0.1
+        m_algParamInitVarianceAccY = joTraceParam.value("InitVarianceACCY").toString().toDouble();              //!< 初始方差-Y轴加速度<initVarianceAccY>：0.1
+        m_algParamInitVarianceAccZ = joTraceParam.value("InitVarianceACCZ").toString().toDouble();              //!< 初始方差-Z轴加速度<initVarianceAccZ>：0.0
+        m_algParamInitVarianceHeading = joTraceParam.value("InitVarianceHeading").toString().toDouble();        //!< 初始方差-方向<initVarianceHeading>：2.46
+        m_algParamInitVarianceHeadingRate = joTraceParam.value("InitVarianceHeadingRate").toString().toDouble();//!< 初始方差-方向变化率<initVarianceHeadingRate>：0.1
 
         ret = true;
     } while (false);
--- examples/Robot1/CDataProcessor.h
+++ examples/Robot1/CDataProcessor.h
@@ -5,12 +5,16 @@
 #include "CPluginContext.h"
 #include "IInterfaceManager.h"
 #include <QJsonValue>
-
+#include <QTimer>
+#include <QObject>
 #include "CRedisClient.h"
 #include "Tracking.hpp"
 
-class CDataProcessor : public IDataProcessor
+class CDataProcessor
+        : public QObject
+        , public IDataProcessor
 {
+    Q_OBJECT
 public:
     explicit CDataProcessor(CPluginContext* pContext=nullptr, IInterfaceManager* pInterfaceManager=nullptr);
     virtual ~CDataProcessor();
@@ -21,6 +25,12 @@ public:
 
 public:
     bool ProcessData(const QByteArray& in) override;
+
+private slots:
+    void slotForTimeout();
+
+private:
+    bool UpdateTrackParams();
 
 private:
     CPluginContext* m_pContext{nullptr};
@@ -33,6 +43,23 @@ private:
     std::vector<PV_OBJ_DATA> inSet;
 
     CRedisClient* m_pRedisClient{nullptr};
+    QTimer*         m_pTimer{nullptr};
+
+private:
+    bool        m_trackVisible{false};                  //!< 跟踪是否可见，是否执行跟踪计算
+    QString     m_algParamDistanceVector{""};           //!< 距离向量 缺省是X，Y值，可选 X轴速度，Y轴速度，强度。
+    double      m_algParamDistanceLimit{0.0};           //!<
+    double      m_algParamInitVariancePosX{0.1};        //!< 初始方差-X轴<initVariancePosX>：0.1
+    double      m_algParamInitVariancePosY{0.1};        //!< 初始方差-Y轴<initVariancePosY>：0.1
+    double      m_algParamInitVariancePosZ{0.0};        //!< 初始方差-Z轴<initVariancePosZ>：0.0
+    double      m_algParamInitVarianceVelX{10.0};       //!< 初始方差-X轴速度<initVarianceVelX>：10
+    double      m_algParamInitVarianceVelY{10.0};       //!< 初始方差-Y轴速度<initVarianceVelY>：10
+    double      m_algParamInitVarianceVelZ{0.0};        //!< 初始方差-Z轴速度<initVarianceVelZ>：0.0
+    double      m_algParamInitVarianceAccX{0.1};        //!< 初始方差-X轴加速度<initVarianceAccX>：0.1
+    double      m_algParamInitVarianceAccY{0.1};        //!< 初始方差-Y轴加速度<initVarianceAccY>：0.1
+    double      m_algParamInitVarianceAccZ{0.0};        //!< 初始方差-Z轴加速度<initVarianceAccZ>：0.0
+    double      m_algParamInitVarianceHeading{2.46};    //!< 初始方差-方向<initVarianceHeading>：2.46
+    double      m_algParamInitVarianceHeadingRate{0.1}; //!< 初始方差-方向变化率<initVarianceHeadingRate>：0.1
 };
 
 #endif // CDATAPROCESSOR_H
